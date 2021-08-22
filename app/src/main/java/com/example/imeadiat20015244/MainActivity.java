d7a91404bb829b3174bcf489e0f10ff0d06f1acd
Udara Sandaruwan <udarasandaruwan96@gmail.com>
2021-08-22 17:44:16 +0530

    initial

---
+++ app/src/main/java/com/example/imeadiat20015244/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.imeadiat20015244;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private Button  btnStart;
+    private Button  btnStop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnStart = findViewById(R.id.StartMusic);
+        btnStop = findViewById(R.id.StopMusic);
+
+
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startService(new Intent(MainActivity.this, MyServices.class));
+            }
+        });
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                stopService(new Intent(MainActivity.this, MyServices.class));
+            }
+        });
+
+
+//        MediaPlayer player =
+//                MediaPlayer.create(this, Settings.System.DEFAULT_NOTIFICATION_URI);
+//        player.setLooping(true);
+//        player.start();
+    }
+
+
+
+
+
+
+    }
